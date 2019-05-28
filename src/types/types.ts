eaa71377b5c0ee651cfa61a07aa8430d00a4de67
SlashArash <arash.kadkhodaei@gmail.com>
2019-05-28 10:27:21 +0430

    add all types

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export const enum dataStatus {
+  NotLoaded = 'NotLoaded',
+  Loading = 'Loading',
+  Loaded = 'Loaded',
+}
+
+export type colorTypes =
+  | 'black'
+  | 'blue'
+  | 'smoke'
+  | 'purple'
+  | 'navyBlue'
+  | 'red';
+
+export type deviceType = '1' | '2' | '16';
