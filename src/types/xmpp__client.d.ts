48952adf0c384817949a0203d21401d8c782bdff
SlashArash <arash.kadkhodaei@gmail.com>
2019-05-28 17:32:19 +0430

    update type definition for xmpp__client

--- src/types/xmpp__client.d.ts
+++ src/types/xmpp__client.d.ts
@@ -1,1 +1,13 @@
-declare module '@xmpp/client'
+declare module '@xmpp/client' {
+  export interface IClient {
+    on: (event: string, callback: any) => Promise<any>;
+    start: () => Promise<any>;
+    send: (message: string) => Promise<any>;
+  }
+  export function client(options: any): IClient;
+  export function xml(
+    name: string,
+    attrs?: string | { [key: string]: any },
+    ...children: xml.ElementChild[]
+  ): xml.Element;
+}
