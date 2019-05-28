1fa31ed87abd0ba3d5e141c1410324a05085fa04
SlashArash <arash.kadkhodaei@gmail.com>
2019-05-28 17:31:44 +0430

    make ProtectedRoute dynamic

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,24 +1,39 @@
-import React, { StatelessComponent } from "react";
-import { Route, Redirect } from "react-router-dom";
+import React, { StatelessComponent } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect, MapStateToProps } from 'react-redux';
+import IStore from 'types/IStore';
 
-export const ProtectedRoute: StatelessComponent<any> = ({
+interface IStateProps {
+  authorized: boolean;
+}
+
+const ProtectedRoute: StatelessComponent<any> = ({
+  authorized,
   component: Component,
-  unauthenticated,
   ...rest
 }) => (
   <Route
     {...rest}
-    render={props =>
-      unauthenticated ? (
+    render={(props) =>
+      authorized ? (
         <Component {...props} />
       ) : (
         <Redirect
           to={{
-            pathname: "/login",
-            state: { from: props.location }
+            pathname: '/login',
+            state: { from: props.location },
           }}
         />
       )
     }
   />
 );
+
+const mapStateToProps: MapStateToProps<IStateProps, any, IStore> = (state) => {
+  const userName = state.app.userName;
+  const password = state.app.password;
+  const authorized = !!userName && !!password;
+  return { authorized };
+};
+
+export const connectedComponent = connect(mapStateToProps)(ProtectedRoute);
